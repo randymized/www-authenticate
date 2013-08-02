a7fa42d4957e3cf512bb15bd0cd7560e084f8e0c
Randy McLaughlin <ot40ddj02@sneakemail.com>
2013-08-01 22:41:09 -0500

    Add parser for Authentication-Info header

--- test/www-authenticate.test.js
+++ test/www-authenticate.test.js
@@ -1,7 +1,9 @@
 
 'use strict';
 require('should');
-var www_authenticate = require('..');
+var www_authenticate = require('..')
+  , parsers = require('../lib/parsers')
+  ;
 
 var CNONCE='0a4f113b'
 
@@ -56,6 +58,25 @@ describe( 'www-authenticate', function() {
           done();
         }
       );
+    } );
+    it( 'should parse an Authentication_Info header', function(done) {
+      var header_parser= www_authenticate("Mufasa","Circle Of Life",{cnonce:CNONCE})
+      var parsed= new parsers.Authentication_Info(
+                 'nextnonce="dcd98b7102dd2f0e8b11d0f600bfb0c093", '+
+                 'qop="auth,auth-int", '+
+                 'rspauth="6629fae49393a05397450978507c4ef1", '+
+                 'nc=00000001, '+
+                 'cnonce="abc""def"'
+              );
+      parsed.should.not.have.property('err');
+      var parms= parsed.parms;
+      parms.should.have.property('nextnonce','dcd98b7102dd2f0e8b11d0f600bfb0c093');
+      parms['nextnonce'].should.equal('dcd98b7102dd2f0e8b11d0f600bfb0c093');
+      parms['qop'].should.equal('auth,auth-int');
+      parms['rspauth'].should.equal('6629fae49393a05397450978507c4ef1');
+      parms['nc'].should.equal('00000001');
+      parms['cnonce'].should.equal('abc"def');  // double double quote properly parsed as single double quote
+      done();
     } );
   } );
 } );
